8255f228d12657be0b24b0fde9370c26508e92d7
Farhaan Bukhsh <farhaan.bukhsh@gmail.com>
2016-12-16 17:32:19 +0530

    First cut of the neural network
    Have initialized the class and added random weights in the network
    the query funtion is almost done too.

---
+++ neural_network.py
@@ -0,0 +1,45 @@
+import numpy
+import scipy.special
+
+
+class Neural_Network:
+    """ This is the first implementation of the Neural_Network
+    """
+    def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
+        """ Initializing the neural network
+        """
+        self.inodes = input_nodes
+        self.hnodes = hidden_nodes
+        self.onodes = output_nodes
+        self.learning_rate = learning_rate
+
+        # This is the heart of  the neural network where the link-weight are
+        # assigned to the nodes for the first time
+        self.wih = numpy.random.normal(0.0, pow(self.hnodes, -0.5),
+                                       (self.hnodes, self.inodes))
+
+        self.who = numpy.random.normal(0.0, pow(self.onodes, -0.5),
+                                       (self.onodes, self.hnodes))
+        # This is the activation funtion i.e sigmond function for crossing the
+        # thereshold value just like in neurons
+        self.activation_function = lambda x: scipy.special.expit(x)
+
+    def train(self, input_list):
+        """ To train the neural network
+        """
+        input_2darray = numpy.array(input_list, ndmin=2).T
+        hidden_input = numpy.dot(self.wih, input_2darray)
+        hidden_output = self.activation_function(hidden_input)
+        final_input = numpy.dot(self.who, hidden_output)
+        final_output = self.activation_function(final_input)
+
+    def query(self, input_list):
+        """ To query the neural network
+        """
+        input_2darray = numpy.array(input_list, ndmin=2).T
+        hidden_input = numpy.dot(self.wih, input_2darray)
+        hidden_output = self.activation_function(hidden_input)
+        final_input = numpy.dot(self.who, hidden_output)
+        final_output = self.activation_function(final_input)
+
+        return final_output
